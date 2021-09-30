fa96009b2dbd6a2e61f46f8bb1b39838409c3267
paulb2299 <79669253+paulb2299@users.noreply.github.com>
2021-09-30 15:18:11 -0500

    Update fcn.py

--- classifiers/fcn.py
+++ classifiers/fcn.py
@@ -63,7 +63,7 @@ class Classifier_FCN:
 			exit()
 		# x_val and y_val are only used to monitor the test loss and NOT for training  
 		batch_size = 16
-		nb_epochs = 5 #Establecemos el numero de epocas que deseemos que ejecute el modelo
+		nb_epochs = 10 #Establecemos el numero de epocas que deseemos que ejecute el modelo
 
 		mini_batch_size = int(min(x_train.shape[0]/10, batch_size))
 
