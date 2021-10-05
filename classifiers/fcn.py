659615706e8e75bff2424a2dbe7cb1ef86d36116
paulb2299 <79669253+paulb2299@users.noreply.github.com>
2021-10-05 00:30:20 -0500

    Update fcn.py

--- classifiers/fcn.py
+++ classifiers/fcn.py
@@ -24,17 +24,19 @@ class Classifier_FCN:
 	def build_model(self, input_shape, nb_classes):
 		input_layer = keras.layers.Input(input_shape) #Empleamos la biblioteca keras para trabajar sobre tensorflow, está implementada en las 3 capas convolucionales
 
+
 		conv1 = keras.layers.Conv1D(filters=128, kernel_size=8, padding='same')(input_layer)
 		conv1 = keras.layers.BatchNormalization()(conv1)
-		conv1 = keras.layers.Activation(activation='relu')(conv1)
+		conv1 = keras.layers.LeakyReLU(alpha=0.3)(conv1)
 
 		conv2 = keras.layers.Conv1D(filters=256, kernel_size=5, padding='same')(conv1)
 		conv2 = keras.layers.BatchNormalization()(conv2)
-		conv2 = keras.layers.Activation('relu')(conv2)
+		conv2 = keras.layers.LeakyReLU(alpha=0.3)(conv2)
 
 		conv3 = keras.layers.Conv1D(128, kernel_size=3,padding='same')(conv2)
 		conv3 = keras.layers.BatchNormalization()(conv3)
-		conv3 = keras.layers.Activation('relu')(conv3)
+		conv3 = keras.layers.LeakyReLU(alpha=0.3)(conv3)
+
 
 		gap_layer = keras.layers.GlobalAveragePooling1D()(conv3)
 
@@ -62,8 +64,8 @@ class Classifier_FCN:
 			print('error')
 			exit()
 		# x_val and y_val are only used to monitor the test loss and NOT for training  
-		batch_size = 16
-		nb_epochs = 7 #Establecemos el numero de epocas que deseemos que ejecute el modelo
+		batch_size = 8
+		nb_epochs = 10 #Establecemos el numero de epocas que deseemos que ejecute el modelo
 
 		mini_batch_size = int(min(x_train.shape[0]/10, batch_size))
 
